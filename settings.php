b820586cd0a29fa43db9f66c890f0092129f2bfe
jerryko <lastnowember@gmail.com>
2016-10-05 00:13:42 +0300

    create file settings for connect to DB

---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php 
+  $base_server = "localhost"; //сервер 
+  $base_name = "ca98472_test"; // имя базы
+  $base_login = "ca98472_test"; // логин пользователя базы
+  $base_pass = "diary36"; // пароль
+?>
